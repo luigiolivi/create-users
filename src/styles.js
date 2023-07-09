b9c12b4fa0d2f457a22431c8aa8d0d48717fa87e
luigiolivi <eterowiczluigi@gmail.com>
2023-07-09 16:28:59 -0300

    structure

---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    background-color: red;
+`;
+
+export const Image = styled.img`
+
+`;
+
+export const ContainerItems = styled.img`
+
+`;
+
+export const H1 = styled.h1`
+    color: white;
+    font-size: 20px;
+`
+
+export const InputLabel = styled.p`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
